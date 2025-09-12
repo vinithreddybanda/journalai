9362fe7d55a8353646e0f4d4e2a46c3cff11fec3
vinithreddybanda <vinithreddy08.123@gmail.com>
2025-09-12 13:56:24 +0530

    fix: remove redundant output in Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
     System.out.println("bye");
+    System.out.println("check");
   }
 }
